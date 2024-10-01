8309032726aea8fa9feea88a7c3e98113aa6116b
SeriousDuck <zotz321@gmail.com>
2024-10-01 12:26:14 +0300

    added content in style.css and script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+const searchBox = document.querySelector(".search-box");
+const searchIcon = document.querySelector("#search-icon");
+
+searchIcon.addEventListener("click", () => {
+    searchBox.classList.toggle("active");
+    navbar.classList.remove("active");
+})
+
+
+// burger menu
+const navbar = document.querySelector(".navbar");
+const menuIcon = document.querySelector("#menu-icon");
+
+menuIcon.addEventListener("click", () => {
+    navbar.classList.toggle("active");
+    searchBox.classList.remove("active");
+})
+
+
+// close nav menu, serch box when scroll
+window.onscroll = () => {
+    navbar.classList.remove("active");
+    searchBox.classList.remove("active");
+}
+
+
+// Header
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+    header.classList.toggle("shadow", window.scrollY > 0);
+})
+
