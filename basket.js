60386f63e3dff2e21ec1e37a028019cb8f9facea
SeriousDuck <zotz321@gmail.com>
2024-10-10 14:27:56 +0300

    changes basket css

--- basket.js
+++ basket.js
@@ -10,10 +10,38 @@ basketIcon.addEventListener("click", () => {
 })
 
 const removeProductFromBasket = (event) => {
-    event.target.parentElemetn.parentElemetn.remove();
-    const basketListItem = document.getElementsByClassName()
+    // event.target.parentElemetn.parentElemetn.remove();
+    const basketListItem = document.getElementsByClassName("basket-list-item");
+    // if(!basketListItem.length) {
+    //     const basketList = document.querySelector(".basket-list");
+    //     basketList.style.display = "none";
+    // }\
+    
+    removeBtn.addEventListener("click", () => {
+        basketList.remove(basketListItem);
+    })
 }
 
-const addToBasket = (product) => {
+const addProductsToBasket = () => {
+    const addBtn = document.querySelector(".content-box > a");
+    // create elements
+    const listProduct = document.createElement("li");
+    const imageProduct = document.createElement("img");
+    const titleProduct = document.createElement("h4");
+    const sectionPriceProduct = document.createElement("section");
+    const priceProduct = document.createElement("span");
+    const removeBtn = document.createElement("button");
 
+    // adding css value
+    listProduct.classList.add("basket-list-item");
+    imageProduct.classList.add("basket-list-img-section > img");
+    titleProduct.classList.add("basket-list-img-section > h4");
+    priceProduct.classList.add("basket-list-price-section > span")
+    removeBtn.classList.add("basket-list-quattity-section > button");
+
+    sectionPriceProduct.append(priceProduct, removeBtn);
+
+
+    // adding value elements
+    imageProduct.parentElement(".box > img");
 }
